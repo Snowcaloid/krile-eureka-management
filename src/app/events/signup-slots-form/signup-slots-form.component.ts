6385974a6f7eff2684170629b566129785ba4c6f
Valentin Baron <valentin.baron@outlook.com>
2024-12-04 17:29:30 +0100

    temp

--- src/app/events/signup-slots-form/signup-slots-form.component.ts
+++ src/app/events/signup-slots-form/signup-slots-form.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-signup-slots-form',
-  imports: [MatFormFieldModule, MatInputModule],
+  imports: [MatFormFieldModule, MatInputModule, MatSelectModule, CommonModule],
   templateUrl: './signup-slots-form.component.html',
   styleUrl: './signup-slots-form.component.css'
 })
@@ -12,4 +14,6 @@ export class SignupSlotsFormComponent {
   current = {
     id: 1
   }
+  parties = [1,2,3,4,5,6,"Support"]
+  slots = [1,2,3,4,5,6,7,8]
 }
